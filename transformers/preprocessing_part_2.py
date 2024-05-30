5780f794f760f9f98a68a7c575755c73ac3fa46b
Kasia Rogalska <kasiarogal03@gmail.com>
2024-05-30 14:58:38 +0200

    Pca start

--- transformers/preprocessing_part_2.py
+++ transformers/preprocessing_part_2.py
@@ -1,5 +1,7 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 
 
 class MoreColumnsRemover(BaseEstimator,TransformerMixin):
@@ -16,8 +18,44 @@ class MoreColumnsRemover(BaseEstimator,TransformerMixin):
 class CategoryEncoder(BaseEstimator,TransformerMixin):
     def __init__(self):
         self.type_column = ['main_category']
+        self.encoder = OneHotEncoder(sparse=False)
+    def fit(self,X,y=None):
+        self.encoder.fit(X[self.type_column])
+        return self
+    def transform(self,X):
+        types_encoded = self.encoder.transform(X[self.type_column])
+        df_encoded = pd.DataFrame(types_encoded, columns=self.encoder.get_feature_names_out(self.type_column))
+        X = pd.concat([X, df_encoded], axis=1).drop(columns=self.type_column)
+        return X
+    def set_output(self, *args, **kwargs):
+        return self
+    
+class BooleanToNumericEncoder(BaseEstimator,TransformerMixin):
+    def __init__(self):
+        self.boolean_column = 'verified'
     def fit(self,X,y=None):
         return self
     def transform(self,X):
-        X=pd.get_dummies(X, columns=self.type_column).astype(int)
+        X[self.boolean_column] = X[self.boolean_column].apply(lambda x: 1 if x else 0)
         return X
+    def set_output(self, *args, **kwargs):
+        return self
+    
+class SecondStandardizer(BaseEstimator,TransformerMixin):
+    def __init__(self):
+        self.scaler = StandardScaler()
+
+    def fit(self, X, y=None):
+        self.scaler.fit(X)
+        return self
+
+    def transform(self, X):
+        scaled_data = self.scaler.transform(X)
+        return scaled_data
+    def set_output(self, *args, **kwargs):
+        return self
+
+    
+
+
+
